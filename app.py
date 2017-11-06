716b06bb9b2b2d3aa726eedd6eb796ef6ed55210
JordonPhillips <phjordon@amazon.com>
2017-11-06 13:24:18 -0800

    Move to personal account and encrypt environment
    This moves things over to my personal account to make use of kms to
    encrypt the app key (and other coming sensitive info) at rest. Keys
    have been rotated so that older keys in this git history are no
    longer valid.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
+import base64
 
 from chalice import Chalice
 from sodapy import Socrata
+import boto3
 
 ANIMAL_DATASET = 'yaai-7frk'
+ENDPOINT = 'data.kingcounty.gov'
+_SOCRATA_CLIENT = None
 
 app = Chalice(app_name='dog-tracker')
-data_client = Socrata('data.kingcounty.gov', os.environ.get('APP_TOKEN'))
+kms = boto3.client('kms')
 
 
 @app.route('/')
@@ -17,7 +21,8 @@ def adoptable_dogs():
 
 @app.route('/count')
 def color_count():
-    colors = app.current_request.query_params.get('colors')
+    params = app.current_request.query_params or {}
+    colors = params.get('colors')
     dog_data = get_adoptable_dogs()
     if not colors:
         return len(dog_data)
@@ -28,8 +33,22 @@ def color_count():
                if color in colors)
 
 
+def get_socrata_client():
+    if _SOCRATA_CLIENT is not None:
+        return _SOCRATA_CLIENT
+    return _setup_socrata_client()
+
+
+def _setup_socrata_client():
+    global _SOCRATA_CLIENT
+    blob = base64.b64decode(os.environ['APP_TOKEN'])
+    app_token = kms.decrypt(CiphertextBlob=blob)['Plaintext']
+    _SOCRATA_CLIENT = Socrata(ENDPOINT, app_token)
+    return _SOCRATA_CLIENT
+
+
 def get_adoptable_dogs():
-    return data_client.get(
+    return get_socrata_client().get(
         ANIMAL_DATASET, record_type='ADOPTABLE', animal_type='Dog'
     )
 
