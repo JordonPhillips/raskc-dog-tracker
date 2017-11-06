a523576219921294765d14f705e11a03fdb5cb6b
JordonPhillips <phjordon@amazon.com>
2017-11-06 10:27:59 -0800

    Get data requests working

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
+import os
+
 from chalice import Chalice
+from sodapy import Socrata
+
+ANIMAL_DATASET = 'yaai-7frk'
 
 app = Chalice(app_name='dog-tracker')
+data_client = Socrata('data.kingcounty.gov', os.environ.get('APP_TOKEN'))
 
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    return get_adoptable_dogs()
 
+def get_adoptable_dogs():
+    return data_client.get(
+        ANIMAL_DATASET, record_type='ADOPTABLE', animal_type='Dog'
+    )
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
